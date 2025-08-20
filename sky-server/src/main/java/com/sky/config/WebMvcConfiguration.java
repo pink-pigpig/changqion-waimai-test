5ad5543fcebd8bfc4c01a172bc721166a2112de9
pink-pigpig <1437689804@qq.com>
2025-08-21 02:40:59 +0800

    feat(user): 新增用户地址簿功能
    - 添加 AddressBookController、AddressBookMapper、AddressBookService等相关类
    - 实现用户地址簿的增删查改功能
    - 添加默认地址设置和查询功能
    - 优化日志输出
    - 修复拦截器失效bug

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -43,20 +43,13 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         registry.addInterceptor(jwtTokenAdminInterceptor)
                 .addPathPatterns("/admin/**")
                 .excludePathPatterns("/admin/employee/login");
-    }
-
-    /**
-     * 注册自定义拦截器
-     *
-     * @param registry
-     */
-    protected void addUserInterceptors(InterceptorRegistry registry) {
         log.info("开始注册自定义拦截器...");
         registry.addInterceptor(jwtTokenUserInterceptor)
                 .addPathPatterns("/user/**")
                 .excludePathPatterns("/user/user/login")
                 .excludePathPatterns("/user/shop/status");
     }
+
 
     /**
      * 通过knife4j生成接口文档
